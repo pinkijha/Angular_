7f9c6c385c454153dc9c92f8ca61c3e4f59155ed
“pinki <pinkijha1310@gmail.com>
2024-03-31 12:16:52 +0530

    2nd commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SubjectComponent } from './subject/subject.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { ContentComponent } from './content/content.component';
+import { Content1Component } from './content1/content1.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SubjectComponent,
+    HeaderComponent,
+    FooterComponent,
+    ContentComponent,
+    Content1Component
   ],
   imports: [
     BrowserModule,
